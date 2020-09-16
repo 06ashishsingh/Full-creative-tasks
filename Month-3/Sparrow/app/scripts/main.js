301b9ba78cdf62ea07401b601e8552f86627d1a0
Ashish Singh <6ashishsingh@gmail.com>
2020-09-16 17:08:50 +0530

    updated styles and script

--- Month-3/Sparrow/app/scripts/main.js
+++ Month-3/Sparrow/app/scripts/main.js
@@ -1,46 +1,49 @@
-const sidebarMailOptions = document.querySelectorAll('.mail-options__item');
+;const sidebarMailOptions = document.querySelectorAll('.mail-options__item');
 const sidebarChatOptions = document.querySelectorAll('.chat-options__item');
-const chatDropdown = document.getElementById('chat-dropdown');
-
-const searchInput = document.getElementById('search-input');
-const searchList = document.querySelector('.search-list');
-
+const clientChatDropdown = document.getElementById('chat-dropdown');
+const clientSubList = document.querySelector('.client-sublist');
+const searchDropdown = document.getElementById('searchDropdown');
+const searchDropdownList = document.querySelector('.search__list');
+const searchInput = document.querySelector('.search__input');
 const mailListItem = document.querySelectorAll('.mail-list__item');
-
-const chatIcons = document.querySelector('.chat__icons');
-const chatIconsList = chatIcons.querySelectorAll('.fa');
-
+const chatIconsList = document.querySelectorAll('.chat__icons_item');
 const sender = document.querySelector('.sender');
 const senderDeatils = document.querySelector('.sender__details');
 
+searchDropdown.addEventListener('click',function(){
+    searchDropdownList.classList.toggle('search__list-show');
+}); 
+
+searchInput.addEventListener('click',function(){
+    searchDropdownList.classList.add('search__list-show');
+})
 
 
-active(sidebarMailOptions,'mail-options-active');
-active(mailListItem,'mail-list-active');
-active(chatIconsList,'chat-icons-active');
-chatListActive();
+mailOptionsActive();
+chatOptionsActive();
+mailListActive();
+chatIconsActive();
 
-function active(items,activeClass){
+function mailOptionsActive(){
 
-    for(let i=0;i<items.length;i++)
+    for(let i=0;i<sidebarMailOptions.length;i++)
     {
-        items[i].addEventListener('click',function(){
+        sidebarMailOptions[i].addEventListener('click',function(){
 
-            for(let j=0;j<items.length;j++)
+            for(let j=0;j<sidebarMailOptions.length;j++)
             {
                 if(j!=i){
-                    items[j].classList.remove(activeClass);
+                    sidebarMailOptions[j].classList.remove('mail-options-active');
                 }
             }
             
-            this.classList.toggle(activeClass);
+            this.classList.toggle('mail-options-active');
         });
     }
-
 }
 
 
-function chatListActive(){
+function chatOptionsActive(){
 
     for(let i=0;i<sidebarChatOptions.length;i++)
     {
@@ -54,40 +57,73 @@ function chatListActive(){
             }
             
             this.classList.toggle('chat-options-active');
-
-            if(i==1)
-            {
-                let clientSubList = document.querySelector('.client-sublist');
-
-                if(clientSubList.style.maxHeight){
-                    clientSubList.style.maxHeight = null;
-                }
-                else{
-                    clientSubList.style.maxHeight = clientSubList.scrollHeight + "px";
-                }
-            }
-
         });
     }
 }
 
+function mailListActive(){
+    
+    for(let i=0;i<mailListItem.length;i++)
+    {
+        mailListItem[i].addEventListener('click',function(){
 
-searchInput.addEventListener('click',function(){
-    searchList.classList.add('search-list-show')
-})
+            for(let j=0;j<mailListItem.length;j++)
+            {
+                if(j!=i){
+                    mailListItem[j].classList.remove('mail-list-active');
+                }
+            }
+            
+            this.classList.toggle('mail-list-active');
+        });
+    }
+}
+
+function chatIconsActive(){
+
+    for(let i=0;i<chatIconsList.length;i++)
+    {
+        chatIconsList[i].addEventListener('click',function(){
+
+            this.classList.toggle('chat-icons-active');
+        })
+    }
+}
+
+
+clientChatDropdown.addEventListener('click',function(event){
+
+    event.stopPropagation();
+
+    if(clientSubList.style.maxHeight)
+    {
+        clientSubList.style.maxHeight = null;
+    }
+    else
+    {
+        clientSubList.style.maxHeight = clientSubList.scrollHeight + 'px';
+    }
+    
+});
 
 sender.addEventListener('click',function(){
-    senderDeatils.classList.add('sender-details-show');
+
+    senderDeatils.classList.toggle('sender-details-show');
+
 });
 
 window.onclick = function(event){
-    
-    if(event.target != sender)
+
+    if(event.target!=searchDropdown && event.target!=searchInput)
+    {
+        searchDropdownList.classList.remove('search__list-show');
+    }
+
+    if(event.target!=sender)
     {
         senderDeatils.classList.remove('sender-details-show');
     }
-    if(event.target != searchInput)
-    {
-        searchList.classList.remove('search-list-show');
-    }
 }
+
+
+
